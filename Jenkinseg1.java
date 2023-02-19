286ab03c85d71a96de2ef0f12ba369ea13b0d29e
Saravanakumar Rajagopal <113620903+sarvo22@users.noreply.github.com>
2023-02-19 12:00:08 +0000

    Update Jenkinseg1.java

--- Jenkinseg1.java
+++ Jenkinseg1.java
@@ -2,6 +2,7 @@ public class Jenkinseg1 {
  public static void main(String[] args) {
   System.out.println("Hello Sarvo");
   System.out.println("Hello Sasi");
+  System.out.println("Hello Arun");
  }
 
 }
